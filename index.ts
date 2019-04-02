ba66d484fd0a4c29ab04545f8a3a7f95a102aa1f
Tomas Voslar <t.voslar@gmail.com>
2019-04-02 09:06:40 +0200

    Initial commit.

---
+++ index.ts
@@ -0,0 +1,37 @@
+import { createLogger, stdSerializers } from 'bunyan'
+import * as stream from 'stream'
+import * as Sentry from '@sentry/node'
+
+Sentry.init({ dsn: process.env.SENTRY_DSN })
+
+const getLogLevelBasedOnNodeEnv = () => {
+  if (process.env.NODE_ENV === 'production') return 'info'
+  if (process.env.NODE_ENV === 'staging') return 'debug'
+  if (process.env.NODE_ENV === 'development') return 'trace'
+  if (process.env.NODE_ENV === 'test') return 'trace'
+}
+
+class SentryStream extends stream.Writable {
+  _write(chunk, enc, next) {
+    const message = JSON.parse(chunk.toString())
+    Sentry.captureMessage(`${message.name} - ${message.msg} - line: ${message.src.line}, file: ${message.src.file}, func: ${message.src.func}`, Sentry.Severity.Fatal)
+    next()
+  }
+}
+
+export const getLogger = (serviceName: string) => {
+  const logger = createLogger({
+    name: serviceName,
+    level: getLogLevelBasedOnNodeEnv(),
+    src: process.env.NODE_ENV !== 'production',
+    serializers: { err: stdSerializers.err }
+  })
+
+  logger.addStream({
+    name: 'sentry',
+    stream: new SentryStream(),
+    level: 'fatal'
+  })
+
+  return logger
+}
