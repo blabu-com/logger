730295cc84f68ecbd896a4fd3dc09de208e8d75f
Tomáš Strejček <tomas.strejcek@ghn.cz>
2019-04-16 10:59:23 +0200

    lint

--- serializeContext.ts
+++ serializeContext.ts
@@ -1,14 +1,14 @@
 export function serializeContext(context) {
-    try {
-        const newContext = {}
-        const allowedFields = [ 'user', 'headers', 'requestId' ]
-        for (const prop in context) {
-            if (context.hasOwnProperty(prop) && allowedFields.includes(prop)) {
-                newContext[prop] = context[prop]
-            }
-        }
-        return newContext
-    } catch (err) { // dont cause side effects on error
-        return context
+  try {
+    const newContext = {}
+    const allowedFields = [ 'user', 'headers', 'requestId' ]
+    for (const prop in context) {
+      if (context.hasOwnProperty(prop) && allowedFields.includes(prop)) {
+        newContext[prop] = context[prop]
+      }
     }
+    return newContext
+  } catch (err) { // dont cause side effects on error
+    return context
+  }
 }
