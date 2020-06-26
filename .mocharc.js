f1b4b131f32baebb8dd29b66f6caf7c218b11755
Tomáš Strejček <tomas.strejcek@ghn.cz>
2020-06-26 13:59:36 +0200

    upgrade deps, better ctx serializer

---
+++ .mocharc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bail: true,
+  require: [
+    'source-map-support/register',
+    'ts-node/register'
+  ],
+  t: 15000,
+  exit: true
+}
--- serializeContext.ts
+++ serializeContext.ts
@@ -1,14 +1,24 @@
-export function serializeContext(context) {
+export function serializeContext(ctx) {
   try {
     const newContext = {}
     const allowedFields = [ 'user', 'headers', 'requestId', 'ip' ]
-    for (const prop in context) {
-      if (context.hasOwnProperty(prop) && allowedFields.includes(prop)) {
-        newContext[prop] = context[prop]
+    for (const prop in ctx) {
+      if (ctx.hasOwnProperty(prop) && allowedFields.includes(prop)) {
+        newContext[prop] = ctx[prop]
       }
+    }
+    if (newContext['headers']) {
+      const newHeaders  = {}
+      const allowedHeaderFields  = [ 'x-requestid' ]
+      for (const prop in newContext['headers']) {
+        if (newContext['headers'].hasOwnProperty(prop) && allowedHeaderFields.includes(prop)) {
+          newHeaders[prop] = newContext['headers'][prop]
+        }
+      }
+      newContext['headers'] = newHeaders
     }
     return newContext
   } catch (err) { // dont cause side effects on error
-    return context
+    return ctx
   }
 }
